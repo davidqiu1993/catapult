186bf52f41e41946ba9b7b88383c86049a8fa0db
David Qiu <david@davidqiu.com>
2017-04-12 17:45:16 -0400

    Append model loading and saving feature to simulation catapult linear modeling utility.

--- src_modeling/run_sim_001_modeling_linear.py
+++ src_modeling/run_sim_001_modeling_linear.py
@@ -23,6 +23,8 @@ import random
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
+from keras.models import save_model as keras_save_model
+from keras.models import load_model as keras_load_model
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation
 from keras.optimizers import RMSprop, SGD
@@ -35,11 +37,14 @@ import cma
 
 
 class TCatapultModelLinearSim(object):
-  def __init__(self, abs_dirpath_data=None):
+  def __init__(self, abs_dirpath_model, abs_dirpath_data=None):
     super(TCatapultModelLinearSim, self).__init__()
+
+    self._dirpath_model = abs_dirpath_model
 
     self._dataset = []
     loader_dataset = TCatapultDatasetSim(abs_dirpath=abs_dirpath_data, auto_init=False)
+    self._dirpath_data = loader_dataset._dataset_dirpath
     loader_dataset.load_dataset()
     for entry in loader_dataset:
       if entry['motion'] == 'linear':
@@ -98,6 +103,43 @@ class TCatapultModelLinearSim(object):
           Y.append(Y_i)
 
     self._model.fit(X, Y, batch_size=len(X), nb_epoch=nb_epoch)
+
+  def _train_model(self):
+    training_method   = 'batch' # 'batch', 'minibatch'
+    batch_rounds      = 512
+    minibatch_size    = 64
+    minibatch_rounds  = int(len(self._dataset) / minibatch_size) * int(np.sqrt(len(self._dataset))) * 2 + 1
+    minibatch_epochs  = 8
+
+    ready_input = input('train with ' + training_method + ', ready (Y)?> ')
+    if training_method == 'batch':
+      self._train_model_batch(nb_epoch=batch_rounds, cheat=False, cheat_threshold=50)
+    elif training_method == 'minibatch':
+      for i in range(minibatch_rounds):
+        self._train_model_minibatch(batch_size=minibatch_size, nb_epoch=minibatch_epochs, cheat=False, cheat_threshold=50)
+        print('minibatch: {}/{}'.format(i+1, minibatch_rounds))
+    else:
+      assert(False)
+
+  def _load_model_weights(self, filepath_model_weights):
+    print('load model weights:', filepath_model_weights)
+    self._model.load_weights(filepath_model_weights)
+
+  def _save_model_weights(self):
+    filename_model_weights_save = 'weights_' + '{:%Y%m%d_%H%M%S_%f}'.format(datetime.datetime.now()) + '.h5'
+    filepath_model_weights_save = os.path.abspath(os.path.join(self._dirpath_model, filename_model_weights_save))
+    print('save model weights:', filepath_model_weights_save)
+    self._model.save_weights(filepath_model_weights_save)
+
+  def _load_model(self, filepath_model):
+    print('load model:', filepath_model)
+    self._model = keras_load_model(filepath_model)
+
+  def _save_model(self):
+    filename_model_save = 'model_' + '{:%Y%m%d_%H%M%S_%f}'.format(datetime.datetime.now()) + '.h5'
+    filepath_model_save = os.path.abspath(os.path.join(self._dirpath_model, filename_model_save))
+    print('save model:', filepath_model_save)
+    keras_save_model(self._model, filepath_model_save)
 
   def _validate(self, validation_dataset, cheat_threshold=50):
     X_valid = []
@@ -217,7 +259,7 @@ class TCatapultModelLinearSim(object):
     POS_MIN = 0.00 * math.pi
     POS_MID = 0.50 * math.pi
     POS_MAX = 0.75 * math.pi
-    DURATION_MIN = 0.01
+    DURATION_MIN = 0.05
     DURATION_MAX = 0.60
 
     corrected_pos_init   = pos_init
@@ -225,7 +267,7 @@ class TCatapultModelLinearSim(object):
     corrected_duration   = duration
     
     penalty = 0
-    penalty_factor = 1
+    penalty_factor = 3
     
     min_pos_diff = 0.1 * math.pi
     
@@ -291,48 +333,86 @@ class TCatapultModelLinearSim(object):
     res = cma.fmin(f, [0.1 * math.pi, 0.6 * math.pi, 0.3], 0.5, [args], 
                    popsize=20, tolx=0.001, verb_disp=False, verb_log=0)
 
+    op_pos_init, op_pos_target, op_duration, op_penalty = self._penalize_action(res[0][0], res[0][1], res[0][2])
     optimal_action = {
-      'pos_init': res[0][0],
-      'pos_target': res[0][1],
-      'duration': res[0][2]
+      'pos_init':   op_pos_init,
+      'pos_target': op_pos_target,
+      'duration':   op_duration,
+      'penalty':    op_penalty
+    }
+
+    return optimal_action
+
+  def _optimize_cma_ctrl_loc_land(self, target_loc_land=30.0):
+    args = {
+      'count_iter': 0
+    }
+
+    def f(x, args):
+      print('CMA-ES iteration. (iteration = {}, target_loc_land = {})'.format(args['count_iter'], target_loc_land))
+      args['count_iter'] += 1
+
+      pos_init, pos_target, duration = x
+      print('CMA-ES sample. (pos_init = {}, pos_target = {}, duration = {})'.format(pos_init, pos_target, duration))
+      pos_init, pos_target, duration, penalty = self._penalize_action(pos_init, pos_target, duration)
+      print('Test sample. (pos_init = {}, pos_target = {}, duration = {})'.format(pos_init, pos_target, duration))
+
+      predictions = self._model.predict([[pos_init, pos_target, duration], [pos_init, pos_target, duration]])
+      y_predict = predictions[0]
+      loc_land_predict = y_predict[0]
+
+      loss = np.abs(target_loc_land - loc_land_predict) + penalty
+      print('loss = {}, loc_land_predict = {}, penalty = {}'.format(np.round(loss, 2), np.round(loc_land_predict, 2), np.round(penalty, 2)))
+      print('')
+
+      return loss
+
+    res = cma.fmin(f, [0.1 * math.pi, 0.6 * math.pi, 0.3], 0.5, [args], 
+                   popsize=20, tolx=0.001, verb_disp=False, verb_log=0)
+
+    op_pos_init, op_pos_target, op_duration, op_penalty = self._penalize_action(res[0][0], res[0][1], res[0][2])
+    optimal_action = {
+      'pos_init':   op_pos_init,
+      'pos_target': op_pos_target,
+      'duration':   op_duration,
+      'penalty':    op_penalty
     }
 
     return optimal_action
 
   def run(self):
-    training_method   = 'batch' # 'batch', 'minibatch'
-    batch_rounds      = 512
-    minibatch_size    = 64
-    minibatch_rounds  = int(len(self._dataset) / minibatch_size) * int(np.sqrt(len(self._dataset))) * 2 + 1
-    minibatch_epochs  = 8
+    argc = len(sys.argv)
+    assert(argc == 1 or argc == 2)
 
-    ready_input = input('train with ' + training_method + ', ready (Y)?> ')
-    if training_method == 'batch':
-      self._train_model_batch(nb_epoch=batch_rounds, cheat=False, cheat_threshold=50)
-    elif training_method == 'minibatch':
-      for i in range(minibatch_rounds):
-        self._train_model_minibatch(batch_size=minibatch_size, nb_epoch=minibatch_epochs, cheat=False, cheat_threshold=50)
-        print('minibatch: {}/{}'.format(i+1, minibatch_rounds))
+    should_load_model = (argc == 2)
+    if should_load_model:
+      self._load_model(os.path.abspath(sys.argv[1]))
     else:
-      assert(False)
-    
-    self._validate(self._dataset)
+      self._train_model()
+      self._validate(self._dataset)
+      should_save_model_input = input('save model (Y/n)?> ').strip().lower()
+      if should_save_model_input in ['', 'y']:
+        self._save_model()
 
     while True:
       print('options:')
       print('  (1) plotting prediction errors')
-      print('  (2) optimization with CMA-ES')
-      print('  (3) plotting prediction errors w.r.t. pos')
+      print('  (2) plotting prediction errors w.r.t. pos')
+      print('  (3) throw farther with CMA-ES')
+      print('  (4) control loc_land with CMA-ES')
       print('  (q) quit (default)')
       option_input = input('option (Q/1/2/...)?> ')
-      option = option_input.lower()
+      option = option_input.strip().lower()
       if option == '1':
         self._plot_prediction_errors(self._dataset)
       elif option == '2':
+        self._plot_prediction_errors_pos(self._dataset, duration=0.05)
+      elif option == '3':
         res = self._optimize_cma_throw_farther()
         print(res)
-      elif option == '3':
-        self._plot_prediction_errors_pos(self._dataset, duration=0.05)
+      elif option == '4':
+        res = self._optimize_cma_ctrl_loc_land(target_loc_land=30.0)
+        print(res)
       else: # (q)
         print('Exit the program.')
         quit()
@@ -340,10 +420,11 @@ class TCatapultModelLinearSim(object):
 
 
 if __name__ == '__main__':
-  catapult_name = 'catapult_sim_001'
-  abs_dirpath_data = os.path.abspath(os.path.join(os.path.dirname(__file__), '../data/' + catapult_name))
+  catapult_name = 'sim_001'
+  abs_dirpath_data = os.path.abspath(os.path.join(os.path.dirname(__file__), '../data/catapult_' + catapult_name))
+  abs_dirpath_model = os.path.abspath(os.path.join(os.path.dirname(__file__), '../data/model_' + catapult_name))
 
-  catapultModel = TCatapultModelLinearSim(abs_dirpath_data=abs_dirpath_data)
+  catapultModel = TCatapultModelLinearSim(abs_dirpath_model, abs_dirpath_data=abs_dirpath_data)
   catapultModel.run()
 
 
