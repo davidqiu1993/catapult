20fd094b0a091a4f87d10001f0a187fb0c145a8f
David Qiu <david@davidqiu.com>
2017-04-29 19:48:22 -0400

    Append multitest evaluation to plot_sim_002

--- src_sim/plot_sim_002_linear_thetaT_d.py
+++ src_sim/plot_sim_002_linear_thetaT_d.py
@@ -227,6 +227,146 @@ def _evaluate_online_learning(test_results_dir, dataset_dir):
   plt.show()
 
 
+def _evaluate_online_learning_multitest(test_results_dir, dataset_dir):
+  prefix = 'evaluate_online_learning_multitest'
+  prefix_info = prefix + ':'
+  
+  test_results_list, dataset = _load_for_analysis(test_results_dir, dataset_dir, multiple_test_results=True)
+  
+  # Check loaded test results
+  if len(test_results_list) < 2:
+    print('{} please select at least two test results files'.format(prefix_info))
+    return
+  for test_results in test_results_list:
+    if len(test_results) < 2:
+      print('{} there must be at least two episodes at each online learning process'.format(prefix_info))
+      return
+    if len(test_results) != len(test_results_list[0]):
+      print('{} all online learning processes must have the same episodes'.format(prefix_info))
+      return
+    if test_results[0]['approach'] != test_results_list[0][0]['approach']:
+      print('{} all online learning processes must use the same approach'.format(prefix_info))
+      return
+  
+  # print test samples
+  print('test results: {}'.format(len(test_results_list[0])))
+  print('')
+  
+  # print test result entry structure
+  print('test result entry structure:')
+  entry_items = []
+  max_entry_item_len = 0
+  for o in test_results_list[0][0]:
+    entry_items.append(o)
+    if len(o) > max_entry_item_len:
+      max_entry_item_len = len(o)
+  entry_items.sort()
+  print('  - approach: {}'.format(test_results_list[0][0]['approach']))
+  for i in range(len(entry_items)):
+    o = entry_items[i]
+    if o != 'approach':
+      print(('  - {0: <' + str(max_entry_item_len + 1) + '} {1}').format(o + ':', type(test_results_list[0][0][o])))
+  print('')
+  
+  # add customized plotting keys
+  list_seqkeys = []
+  append_seqkey = None
+  while not append_seqkey == '':
+    append_seqkey_input = input('{} add key (empty to end): '.format(prefix_info)).strip().lower()
+    append_seqkey = str(append_seqkey_input)
+    if len(append_seqkey) > 0:
+      list_seqkeys.append(append_seqkey)
+  
+  # construct episodic data
+  seq_episode_mtlist = []
+  seq_rewards_mtlist = []
+  list_seqs_mtlist = []
+  for test_results in test_results_list:
+    seq_episode = []
+    seq_rewards = []
+    list_seqs = []
+    for seqkey in list_seqkeys:
+      list_seqs.append([])
+    for i in range(len(test_results)):
+      entry = test_results[i]
+
+      # episode sequence
+      cur_episode = int(i)
+      seq_episode.append(cur_episode)
+
+      # rewards sequence
+      cur_rewards = float(- abs(entry['desired_loc_land'] - entry['loc_land']))
+      seq_rewards.append(cur_rewards)
+      
+      # other sequences
+      for i_seq in range(len(list_seqkeys)):
+        cur_value = float(entry[list_seqkeys[i_seq]])
+        list_seqs[i_seq].append(cur_value)
+    
+    seq_episode_mtlist.append(seq_episode)
+    seq_rewards_mtlist.append(seq_rewards)
+    list_seqs_mtlist.append(list_seqs)
+  
+  # construct plotting data
+  mt_count = len(seq_episode_mtlist)
+  seq_episode = seq_episode_mtlist[0]
+  seq_rewards_mean = []
+  seq_rewards_center = []
+  seq_rewards_err = []
+  list_seqs_mean = [[] for j in range(len(list_seqs_mtlist[0]))]
+  list_seqs_center = [[] for j in range(len(list_seqs_mtlist[0]))]
+  list_seqs_err = [[] for j in range(len(list_seqs_mtlist[0]))]  
+  for i in range(len(seq_episode)):
+    reward_acc = 0
+    reward_min = seq_rewards_mtlist[0][0]
+    reward_max = seq_rewards_mtlist[0][0]
+    
+    seqs_acc = [0                             for i_seq in range(len(list_seqs_mtlist[0]))]
+    seqs_min = [list_seqs_mtlist[0][i_seq][0] for i_seq in range(len(list_seqs_mtlist[0]))]
+    seqs_max = [list_seqs_mtlist[0][i_seq][0] for i_seq in range(len(list_seqs_mtlist[0]))]
+    
+    for i_mt in range(mt_count):
+      reward_acc += seq_rewards_mtlist[i_mt][i]
+      if seq_rewards_mtlist[i_mt][i] < reward_min: reward_min = seq_rewards_mtlist[i_mt][i]
+      if seq_rewards_mtlist[i_mt][i] > reward_max: reward_max = seq_rewards_mtlist[i_mt][i]
+      
+      for i_seq in range(len(list_seqs_mtlist[0])):
+        seqs_acc[i_seq] += list_seqs_mtlist[i_mt][i_seq][i]
+        if list_seqs_mtlist[i_mt][i_seq][i] < seqs_min[i_seq]: seqs_min[i_seq] = list_seqs_mtlist[i_mt][i_seq][i]
+        if list_seqs_mtlist[i_mt][i_seq][i] > seqs_max[i_seq]: seqs_max[i_seq] = list_seqs_mtlist[i_mt][i_seq][i]
+    
+    seq_rewards_mean.append(reward_acc / mt_count)
+    seq_rewards_center.append((reward_max + reward_min) / 2)
+    seq_rewards_err.append((reward_max - reward_min) / 2)
+    
+    for i_seq in range(len(list_seqs_mtlist[0])):
+      list_seqs_mean[i_seq].append(seqs_acc[i_seq] / mt_count)
+      list_seqs_center[i_seq].append((seqs_max[i_seq] + seqs_min[i_seq]) / 2)
+      list_seqs_err[i_seq].append((seqs_max[i_seq] - seqs_min[i_seq]) / 2)
+  
+  # figure configuraiton
+  n_subplots = 1 + len(list_seqkeys)
+  plt.figure()
+  
+  # plot: episode - rewards
+  plt.subplot(n_subplots * 100 + 10 + 1)
+  plt.xlabel('episode')
+  plt.ylabel('rewards')
+  plt.plot(seq_episode, seq_rewards_mean, 'b-')
+  plt.errorbar(seq_episode, seq_rewards_center, seq_rewards_err, color='b', linestyle='None')
+  
+  # plot: others
+  for i_seq in range(len(list_seqkeys)):
+    plt.subplot(n_subplots * 100 + 10 + i_seq + 2)
+    plt.xlabel('episode')
+    plt.ylabel(list_seqkeys[i_seq])
+    plt.plot(seq_episode, list_seqs_mean[i_seq], 'b-')
+    plt.errorbar(seq_episode, list_seqs_center[i_seq], list_seqs_err[i_seq], color='b', linestyle='None')
+  
+  # show plot
+  plt.show()
+
+
 def _quit(test_results_dir, dataset_dir):
   quit()
 
@@ -240,13 +380,18 @@ def _getOperations():
     },
     {
       'code': '1',
-      'desc': 'general test result estimation',
+      'desc': 'general test result evaluation',
       'func': _evaluate_test_results
     },
     {
       'code': '2',
-      'desc': 'online learning test result estimation',
+      'desc': 'online learning test result evaluation',
       'func': _evaluate_online_learning
+    },
+    {
+      'code': '3',
+      'desc': 'online learning test result evaluation (multitest)',
+      'func': _evaluate_online_learning_multitest
     }
   ]
   
