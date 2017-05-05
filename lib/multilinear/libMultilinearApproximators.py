65b70d319a72c72888107a4142f5880b0adb8955
David Qiu <david@davidqiu.com>
2017-05-05 19:01:19 -0400

    Update multilinear approximators lib

--- lib/multilinear/libMultilinearApproximators.py
+++ lib/multilinear/libMultilinearApproximators.py
@@ -55,26 +55,41 @@ class TMultilinearApproximators(object):
       y_list = [(self.predictByApproximator(i, x)) for i in range(self._N_APPROXIMATORS)]
       return y_list
   
+  def flush(self):
+    for i in range(self._N_APPROXIMATORS):
+      self._X_train_list[i] = []
+      self._Y_train_list[i] = []
+  
+  def updateForApproximator(self, approximatorIndex, append_X_train, append_Y_train):
+    assert(0 <= approximatorIndex and approximatorIndex < self._N_APPROXIMATORS)
+    assert(len(append_X_train) == len(append_Y_train))
+    
+    for i in range(len(append_X_train)):
+      self._X_train_list[approximatorIndex].append(append_X_train[i])
+      self._Y_train_list[approximatorIndex].append(append_Y_train[i])
+    
+    assert(len(self._X_train_list[approximatorIndex]) == len(self._Y_train_list[approximatorIndex]))
+    if len(self._X_train_list[approximatorIndex]) > 0:
+      slope, intercept, r_value, p_value, std_err = stats.linregress(self._X_train_list[approximatorIndex], self._Y_train_list[approximatorIndex])
+      self._approximators[approximatorIndex][0] = intercept
+      self._approximators[approximatorIndex][1] = slope
+      self._rvalue_list[approximatorIndex] = r_value
+      self._pvalue_list[approximatorIndex] = p_value
+      self._stderr_list[approximatorIndex] = std_err
+    
+    return self._approximators[approximatorIndex], self._rvalue_list[approximatorIndex], self._pvalue_list[approximatorIndex], self._stderr_list[approximatorIndex]
+  
   def update(self, append_X_train_list, append_Y_train_list):
     assert(len(append_X_train_list) == self._N_APPROXIMATORS)
     assert(len(append_Y_train_list) == self._N_APPROXIMATORS)
     
-    for i in range(self._N_APPROXIMATORS):
-      assert(len(append_X_train_list[i]) == len(append_Y_train_list[i]))
-      for j in range(len(append_X_train_list[i])):
-        self._X_train_list[i].append(append_X_train_list[i][j])
-        self._Y_train_list[i].append(append_Y_train_list[i][j])
-      
-      if len(self._X_train_list[i]) > 0:
-        assert(len(self._X_train_list[i]) == len(self._Y_train_list[i]))
-        slope, intercept, r_value, p_value, std_err = stats.linregress(self._X_train_list[i], self._Y_train_list[i])
-        self._approximators[i][0] = intercept
-        self._approximators[i][1] = slope
-        self._rvalue_list[i] = r_value
-        self._pvalue_list[i] = p_value
-        self._stderr_list[i] = std_err
+    for approximatorIndex in range(self._N_APPROXIMATORS):
+      self.updateForApproximator(approximatorIndex, append_X_train_list[approximatorIndex], append_Y_train_list[approximatorIndex])
     
     return self._approximators, self._rvalue_list, self._pvalue_list, self._stderr_list
+  
+  def countApproximators(self):
+    return self._N_APPROXIMATORS
   
   def getApproximators(self):
     return self._approximators
@@ -117,21 +132,30 @@ if __name__ == '__main__':
   import matplotlib.pyplot as plt
   
   n_approximators = 3
-  n_samples = 16
+  n_samples = 5
   mtl = TMultilinearApproximators(0.0, 1.0, n_approximators=n_approximators)
   
   X_train_list = [(np.random.random(n_samples)) for i in range(n_approximators)]
   Y_train_list = [(np.random.random(n_samples)) for i in range(n_approximators)]
   mtl.update(X_train_list, Y_train_list)
   
+  X_train = np.random.random(n_samples)
+  Y_train = np.random.random(n_samples)
+  mtl.updateForApproximator(0, X_train, Y_train)
+  
   X = [0.0, 1.0]
   Y_list = mtl.predict(X)
   color_list = ['r', 'y', 'b']
+  
+  print('X = {}, Y = {}, marker = {}'.format(X_train, Y_train, color_list[0] + 'x'))
+  plt.plot(X_train, Y_train, color_list[0] + 'x')
+  
   for i in range(n_approximators):
-    print X, Y_list[i], color_list[i] + '-'
+    print('X = {}, Y = {}, marker = {}'.format(X_train_list[i], Y_train_list[i], color_list[i] + 'x'))
+    plt.plot(X_train_list[i], Y_train_list[i], color_list[i] + 'x')
+    print('X_pred = {}, Y_pred = {}, marker = {}'.format(X, Y_list[i], color_list[i] + '-'))
     plt.plot(X, Y_list[i], color_list[i] + '-')
-    print X_train_list[i], Y_train_list[i], color_list[i] + 'x'
-    plt.scatter(X_train_list[i], Y_train_list[i], color=color_list[i], marker='x')
+  
   plt.show()
 
 
