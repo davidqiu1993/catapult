c6523a1e04d77340b97edb4333a37c90a027f04e
David Qiu <david@davidqiu.com>
2017-05-08 16:44:47 -0400

    Update multilinear approximators lib for generalization in multi-dimensional input space

--- lib/multilinear/libMultilinearApproximators.py
+++ lib/multilinear/libMultilinearApproximators.py
@@ -10,126 +10,152 @@ __website__   = 'www.davidqiu.com'
 __copyright__ = 'Copyright (C) 2017, David Qiu. All rights reserved.'
 
 
+import math
 import numpy as np
-from scipy import stats
+from sklearn import linear_model
 
 import pdb
 
 
 class TMultilinearApproximators(object):
-  def __init__(self, init_range_min, init_range_max, n_approximators=20):
-    assert(init_range_max > init_range_min)
+  def __init__(self, dim_input=1, n_approximators=20):
+    assert(dim_input > 0)
+    assert(n_approximators > 0)
+    
     super(TMultilinearApproximators, self).__init__()
     
+    self._DIM_INPUT = dim_input
     self._N_APPROXIMATORS = n_approximators
     
-    self._approximators = []
-    self._X_train_list = []
-    self._Y_train_list = []
-    self._rvalue_list = [] # correlation coefficient
-    self._pvalue_list = [] # two-sided p-value for a hypothesis test whose null hypothesis is that the slope is zero
-    self._stderr_list = [] # standard error of the estimated gradient
-    for i in range(self._N_APPROXIMATORS):
-      w0 = float(init_range_min + np.random.sample() * (init_range_max - init_range_min)) # intercept
-      w1 = float(0.0) # slope
-      self._approximators.append([w0, w1])
+    self._approximators = [] # [(coefficients, intercept), ...]
+    self._X_train_list = [] # [[x, ...], ...] where x = vector([x1, x2, ...])
+    self._Y_train_list = [] # [[y, ...], ...] where y = scalar(y)
+    for approximatorIndex in range(self._N_APPROXIMATORS):
+      coefficients = [0.0 for i in range(self._DIM_INPUT)]
+      intercept = 0.0
+      self._approximators.append((coefficients, intercept))
       self._X_train_list.append([])
       self._Y_train_list.append([])
-      self._rvalue_list.append(0.0)
-      self._pvalue_list.append(0.0)
-      self._stderr_list.append(0.0)
   
-  def predictByApproximator(self, approximatorIndex, x):
-    w = self._approximators[approximatorIndex]
-    y = w[0] + w[1] * x
-    return y
-  
-  def predict(self, x):
-    is_batch = hasattr(x, '__iter__')
+  def _train(self, approximatorIndex):
+    assert(len(self._X_train_list[approximatorIndex]) > 0)
+    assert(len(self._X_train_list[approximatorIndex]) == len(self._Y_train_list[approximatorIndex]))
     
-    if is_batch:
-      X = x
-      Y_list = [[(self.predictByApproximator(i, X[j])) for j in range(len(X))] for i in range(self._N_APPROXIMATORS)]
-      return Y_list
-    else:
-      y_list = [(self.predictByApproximator(i, x)) for i in range(self._N_APPROXIMATORS)]
-      return y_list
+    reg = linear_model.LinearRegression()
+    reg.fit(self._X_train_list[approximatorIndex], self._Y_train_list[approximatorIndex])
+    
+    coefficients = reg.coef_.tolist()
+    intercept = float(reg.intercept_)
+    self._approximators[approximatorIndex] = (coefficients, intercept)
+    
+    return self._approximators[approximatorIndex]
   
-  def flushForApproximator(self, approximatorIndex):
+  def _trainAll(self):
+    for approximatorIndex in range(self._N_APPROXIMATORS):
+      self._train(approximatorIndex)
+    
+    return self._approximators
+  
+  def update(self, approximatorIndex, append_X_train, append_Y_train):
     assert(0 <= approximatorIndex and approximatorIndex < self._N_APPROXIMATORS)
+    assert(len(append_X_train) > 0)
+    assert(len(append_X_train) == len(append_Y_train))
+    for i in range(len(append_X_train)):
+      assert(len(append_X_train[i]) == self._DIM_INPUT)
+    
+    self._X_train_list[approximatorIndex] += append_X_train
+    self._Y_train_list[approximatorIndex] += append_Y_train
+    
+    return self._train(approximatorIndex)
+  
+  def updateAll(self, append_X_train_list, append_Y_train_list):
+    for approximatorIndex in range(self._N_APPROXIMATORS):
+      self.update(approximatorIndex, append_X_train_list[approximatorIndex], append_Y_train_list[approximatorIndex])
+    
+    return self._approximators
+  
+  def flush(self, approximatorIndex):
     self._X_train_list[approximatorIndex] = []
     self._Y_train_list[approximatorIndex] = []
   
-  def flush(self):
+  def flushAll(self):
     for approximatorIndex in range(self._N_APPROXIMATORS):
-      self.flushForApproximator(approximatorIndex)
+      self.flush(approximatorIndex)
   
-  def updateForApproximator(self, approximatorIndex, append_X_train, append_Y_train):
-    assert(0 <= approximatorIndex and approximatorIndex < self._N_APPROXIMATORS)
-    assert(len(append_X_train) == len(append_Y_train))
+  def predict(self, approximatorIndex, x):
+    assert(len(x) == self._DIM_INPUT)
     
-    for i in range(len(append_X_train)):
-      self._X_train_list[approximatorIndex].append(append_X_train[i])
-      self._Y_train_list[approximatorIndex].append(append_Y_train[i])
+    w, b = self._approximators[approximatorIndex]
     
-    assert(len(self._X_train_list[approximatorIndex]) == len(self._Y_train_list[approximatorIndex]))
-    if len(self._X_train_list[approximatorIndex]) > 0:
-      slope, intercept, r_value, p_value, std_err = stats.linregress(self._X_train_list[approximatorIndex], self._Y_train_list[approximatorIndex])
-      self._approximators[approximatorIndex][0] = intercept
-      self._approximators[approximatorIndex][1] = slope
-      self._rvalue_list[approximatorIndex] = r_value
-      self._pvalue_list[approximatorIndex] = p_value
-      self._stderr_list[approximatorIndex] = std_err
+    y_h = float(np.dot(np.array(w), np.array(x)) + b)
     
-    return self._approximators[approximatorIndex], self._rvalue_list[approximatorIndex], self._pvalue_list[approximatorIndex], self._stderr_list[approximatorIndex]
+    return y_h
   
-  def update(self, append_X_train_list, append_Y_train_list):
-    assert(len(append_X_train_list) == self._N_APPROXIMATORS)
-    assert(len(append_Y_train_list) == self._N_APPROXIMATORS)
-    
+  def predictAll(self, x):
+    y_h_list = []
     for approximatorIndex in range(self._N_APPROXIMATORS):
-      self.updateForApproximator(approximatorIndex, append_X_train_list[approximatorIndex], append_Y_train_list[approximatorIndex])
+      y_h = self.predict(approximatorIndex, x)
+      y_h_list.append(y_h)
     
-    return self._approximators, self._rvalue_list, self._pvalue_list, self._stderr_list
+    return y_h_list
+  
+  def normalVectorsAngle(self, approximatorIndex_1, approximatorIndex_2):
+    """
+    @return angle in [0.0, pi]
+    """
+    assert(0 <= approximatorIndex_1 and approximatorIndex_1 < self._N_APPROXIMATORS)
+    assert(0 <= approximatorIndex_2 and approximatorIndex_2 < self._N_APPROXIMATORS)
+    
+    coefficients_1, intercept_1 = self._approximators[approximatorIndex_1]
+    coefficients_2, intercept_2 = self._approximators[approximatorIndex_2]
+    
+    nv_1 = np.array([-1] + coefficients_1)
+    nv_2 = np.array([-1] + coefficients_2)
+    cosTheta = np.dot(nv_1, nv_2) / (np.linalg.norm(nv_1) * np.linalg.norm(nv_2))
+    theta = float(np.arccos(cosTheta))
+    
+    return theta
+  
+  def hyperplanesAngle(self, approximatorIndex_1, approximatorIndex_2):
+    """
+    return angle in [0.0, 0.5*pi]
+    """
+    nv_angle = self.normalVectorsAngle(approximatorIndex_1, approximatorIndex_2)
+    hp_angle = float(min(nv_angle, math.pi - nv_angle))
+    
+    return hp_angle
   
   def countApproximators(self):
     return self._N_APPROXIMATORS
   
-  def getApproximators(self):
-    return self._approximators
+  @property
+  def N(self):
+    return self.countApproximators()
+  
+  def __len__(self):
+    return self.countApproximators()
   
   def getApproximator(self, approximatorIndex):
     return self._approximators[approximatorIndex]
   
-  def get_X_train_list(self):
-    return self._X_train_list
+  def __getitem__(self, approximatorIndex):
+    return self.getApproximator(approximatorIndex)
   
-  def get_X_train(self, approximatorIndex):
+  def getAllApproximators(self):
+    return self._approximators
+  
+  def getX(self, approximatorIndex):
     return self._X_train_list[approximatorIndex]
   
-  def get_Y_train_list(self):
-    return self._Y_train_list
-  
-  def get_Y_train(self, approximatorIndex):
+  def getY(self, approximatorIndex):
     return self._Y_train_list[approximatorIndex]
   
-  def get_rvalue_list(self):
-    return self._rvalue_list
+  def getAllX(self):
+    return self._X_train_list
   
-  def get_rvalue(self, approximatorIndex):
-    return self._rvalue_list[approximatorIndex]
-  
-  def get_pvalue_list(self):
-    return self._pvalue_list
-  
-  def get_pvalue(self, approximatorIndex):
-    return self._pvalue_list[approximatorIndex]
-  
-  def get_stderr_list(self):
-    return self._stderr_list
-  
-  def get_stderr(self, approximatorIndex):
-    return self._stderr_list[approximatorIndex]
+  def getAllY(self):
+    return self._Y_train_list
+
 
 
 if __name__ == '__main__':
@@ -137,18 +163,20 @@ if __name__ == '__main__':
   
   n_approximators = 3
   n_samples = 5
-  mtl = TMultilinearApproximators(0.0, 1.0, n_approximators=n_approximators)
+  mtl = TMultilinearApproximators(dim_input=1, n_approximators=n_approximators)
   
-  X_train_list = [(np.random.random(n_samples)) for i in range(n_approximators)]
-  Y_train_list = [(np.random.random(n_samples)) for i in range(n_approximators)]
-  mtl.update(X_train_list, Y_train_list)
+  X_train_list = [[[np.random.random()] for j in range(n_samples)] for i in range(n_approximators)]
+  Y_train_list = [[ np.random.random()  for j in range(n_samples)] for i in range(n_approximators)]
+  mtl.updateAll(X_train_list, Y_train_list)
   
-  X_train = np.random.random(n_samples)
-  Y_train = np.random.random(n_samples)
-  mtl.updateForApproximator(0, X_train, Y_train)
+  X_train = [[np.random.random()] for i in range(n_samples)]
+  Y_train = [ np.random.random()  for i in range(n_samples)]
+  mtl.update(0, X_train, Y_train)
   
-  X = [0.0, 1.0]
-  Y_list = mtl.predict(X)
+  X = [[0.0], [1.0]]
+  y_list_list = [mtl.predictAll(X[i]) for i in range(len(X))]
+  Y_list = [[y_list_list[i][j] for i in range(len(X))] for j in range(len(mtl))]
+  
   color_list = ['r', 'y', 'b']
   
   print('X = {}, Y = {}, marker = {}'.format(X_train, Y_train, color_list[0] + 'x'))
@@ -159,6 +187,10 @@ if __name__ == '__main__':
     plt.plot(X_train_list[i], Y_train_list[i], color_list[i] + 'x')
     print('X_pred = {}, Y_pred = {}, marker = {}'.format(X, Y_list[i], color_list[i] + '-'))
     plt.plot(X, Y_list[i], color_list[i] + '-')
+  
+  nv_angle = 180 * mtl.normalVectorsAngle(0, 1) / math.pi
+  hp_angle = 180 * mtl.hyperplanesAngle(0, 1) / math.pi
+  print('line_red, line_yellow: nv_angle = {}, hp_angle = {}'.format(nv_angle, hp_angle))
   
   plt.show()
 
